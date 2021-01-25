9294f650fdc28eabca3e0724423bda4bc470342b
Sadman_Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-01-25 16:41:49 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Banner/>
       {/* Rows */}
       <Row title="NETFLIX ORIGINALS"  fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
-      <Row title="Tending Now"        fetchUrl={requests.fetchTrending}/>
+      <Row title="Trending Now"        fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated"          fetchUrl={requests.fetchTopRated}/>
       <Row title="Action Movies"      fetchUrl={requests.fetchActionMovies}/>
       <Row title="Comedy Movies"      fetchUrl={requests.fetchComedyMovies}/>
